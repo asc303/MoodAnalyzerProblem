5703769edce0395514149c4ddfe3fda88a8e32a1
asc303 <chauhansam94@gmail.com>
2021-12-02 21:59:01 +0530

    [ashutosh] add:MoodAnalyzerProblem

---
+++ MoodAnalyzerProblem/MoodAnalyzerProblem/MoodAnalyzer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyzer
+    {
+        public string message;
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyzeMood()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.ENTERED_EMPTY, "Mood should not be Empty");
+                }
+                if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
---
+++ MoodAnalyzerProblem/MoodAnalyzerTesting/MoodAnalyzerTest.cs
@@ -0,0 +1,68 @@
+using MoodAnalyzerProblem;
+using NUnit.Framework;
+
+namespace MoodAnalyzerTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+        [Test]
+        public void GivenSadMessage_WhenAnalyze_ShouldReturnSad()
+        {
+            string message = "I am in Sad Mood";
+            string output = "SAD";
+            MoodAnalyzer mood = new MoodAnalyzer(message);
+            string result = mood.AnalyzeMood();
+            Assert.AreEqual(output, result);
+        }
+        [Test]
+        public void GivenAnyMessage_WhenAnalyze_ShouldReturnHappy()
+        {
+            string message = "I am in Any Mood";
+            string output = "HAPPY";
+            MoodAnalyzer mood = new MoodAnalyzer(message);
+            string result = mood.AnalyzeMood();
+            Assert.AreEqual(output, result);
+        }
+        [Test]
+        public void GivenNullMessage_WhenAnalyze_ShouldReturnHappy()
+        {
+            string message = null;
+            string output = "HAPPY";
+            MoodAnalyzer mood = new MoodAnalyzer(message);
+            string result = mood.AnalyzeMood();
+            Assert.AreEqual(output, result);
+        }
+        [Test]
+        public void GivenNullMood_WhenAnalyze_ShouldThrowInvalidMoodException()
+        {
+            try
+            {
+                string message = null;
+                MoodAnalyzer mood = new MoodAnalyzer(message);
+                string result = mood.AnalyzeMood();
+            }
+            catch (MoodAnalyzerException e)
+            {
+                Assert.AreEqual("Mood should not be Null", e.Message);
+            }
+        }
+        [Test]
+        public void GivenEMPTYMood_WhenAnalyze_ShouldThrowInvalidMoodException()
+        {
+            try
+            {
+                string message = "";
+                MoodAnalyzer mood = new MoodAnalyzer(message);
+                string result = mood.AnalyzeMood();
+            }
+            catch (MoodAnalyzerException e)
+            {
+                Assert.AreEqual("Mood should not be Empty", e.Message);
+            }
+        }
+    }
+}
